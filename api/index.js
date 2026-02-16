6ec592bf0e6902e9ad4ca2b05dd63d61aead19c8
Buki101 <dylanmoore12003@gmail.com>
2026-02-16 23:23:58 +1100

    Move OzRust kit catalog off homepage into dedicated page

--- api/index.js
+++ api/index.js
@@ -241,6 +241,10 @@ app.get('/player/:steamId', requireAuthPage, (_req, res) => {
   res.sendFile(path.join(__dirname, '..', 'player.html'));
 });
 
+app.get('/ozrust', requireAuthPage, (_req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'ozrust.html'));
+});
+
 app.get('/subscription/:kitId', requireAuthPage, (_req, res) => {
   res.sendFile(path.join(__dirname, '..', 'subscription.html'));
 });
